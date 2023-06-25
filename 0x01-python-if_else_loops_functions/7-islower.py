f959ea6d1d971bd0598b1703ea1cfc4ab530b966
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-24 17:58:08 -0700

    task7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    """function that decides id the letter
+    is uppercase or lower case"""
+    if (ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
