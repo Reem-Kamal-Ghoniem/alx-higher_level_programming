a83c8ef49c5b3c8cda85421a01143c9c7bf8a93f
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-24 18:33:55 -0700

    task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+check = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - 32)), end="")
+    print("{}".format(chr(c - check)), end="")
+    check = 32 if check == 0 else 0
