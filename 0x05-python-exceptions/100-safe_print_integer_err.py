616df43bbecbf596720689c2e81036398f8d597b
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-26 15:52:03 -0700

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer(value):
     """a function that prints an intege"""
+    import sys
     try:
         print("{:d}".format(value))
         return (True)
