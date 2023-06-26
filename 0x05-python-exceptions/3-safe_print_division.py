42cbb8e334794e5afb0b900ab6ec351c6c24a0c8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-26 15:00:25 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         x = None
     finally:
-        print("Inside result {}".format(x))
+        print("Inside result: {}".format(x))
         return (x)
