d3c0740808be2693669e819188d9ab7866215544
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-26 15:24:44 -0700

    task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     """a function that raises a name exception with a message."""
-    raise Exception(message)
+    raise NameError(message)
