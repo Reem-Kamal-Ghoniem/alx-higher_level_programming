1426c98605b4f5be99ab4eaaf2922cc54752e05c
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-26 15:13:50 -0700

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     """a function that divides element by element 2 lists"""
     newlist = []
-    for i in range list_length
+    for i in range list_length:
         try:
             out = my_list_1[i] / my_list_2[i]
         except TypeError:
