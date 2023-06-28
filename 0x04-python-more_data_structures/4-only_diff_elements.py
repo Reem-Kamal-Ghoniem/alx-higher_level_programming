354b8f243c3e1f167578fff2dd3ed7db8093f492
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 02:06:38 -0700

    task 3.3

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,6 +2,14 @@
 def only_diff_elements(set_1, set_2):
     """a function that returns a set of all
     elements present in only one set."""
-    new_set = set()
-    new_set = set_1 | set_2
-    return (new_set)
+    diff_set = set()
+
+    for element in set_1:
+        if element not in set_2:
+            diff_set.add(element)
+
+    for element in set_2:
+        if element not in set_1:
+            diff_set.add(element)
+
+    return diff_set
