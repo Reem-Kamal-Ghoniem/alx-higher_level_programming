116afbff1ae9f25219357820cee79bf1466a7592
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 01:07:41 -0700

    task 0.1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,9 @@
 def square_matrix_simple(matrix=[]):
     """a function that computes the square
     value of all integers of a matrix"""
+    if not matrix:
+        return None
     new_matrix = []
     for i in range matrix:
-        new_matrix[i] = matrix[i]
+        new_matrix[i] = matrix[i] ** 2
     return (new_matrix)
