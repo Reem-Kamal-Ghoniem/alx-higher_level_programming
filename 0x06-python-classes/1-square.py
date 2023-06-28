31287607e09984c7d328b9bf4e2165452446d1a3
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-27 19:54:34 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""a class Square that defines a square"""
+"""a class Square that define a square and its size"""
 
 
-class square():
-    """a square class"""
+class Square():
+    """Class of a square"""
+
     def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size (int): size of the square
+        """
         self.__size = size
-        pass
-    pass
