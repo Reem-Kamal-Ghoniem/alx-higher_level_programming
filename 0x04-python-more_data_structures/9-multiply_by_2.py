54d106185a3aa7cdd8d7a0f4c3dc10ab8976805a
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 02:40:13 -0700

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """a function that returns a new dictionary
+    with all values multiplied by 2"""
+    for value in a_dictionary
+        a_dictionary[value] *= 2
+    return (a_dictionary)
