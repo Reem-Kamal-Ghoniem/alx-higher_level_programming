4168726364ba7842bce71e94849dade296578f9a
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 02:43:23 -0700

    task 9.2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,9 @@
 def multiply_by_2(a_dictionary):
     """a function that returns a new dictionary
     with all values multiplied by 2"""
-    for value in a_dictionary
-       new = a_dictionary[value] * 2
-    return (new)
+    new_dictionary = {}
+
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
+
+    return new_dictionary
