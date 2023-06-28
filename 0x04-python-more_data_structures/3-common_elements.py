f6f1b62ca20c3556a985d5714f22c13e315e37f2
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 01:55:05 -0700

    task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """a function that returns a set of common elements in two sets."""
+    newset = set()
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                newset.add(i)
+    return (newset)
