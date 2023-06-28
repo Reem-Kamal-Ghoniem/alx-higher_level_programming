798c26794a0c4d88dcf6822816f8b9625c4f7b26
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 01:16:37 -0700

    task 0.3

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,6 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
     new_matrix = []
-    for i in range matrix:
-        new_matrix[i] =list(map(lambda x: x ** 2, matrix[i]))
+    for i in range len(matrix):
+        new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
     return (new_matrix)
