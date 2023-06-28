8d69dd2aa5aa2387cfe953f82cf1449a64bdcb8a
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 01:47:45 -0700

    task 2.1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     """a function that adds all unique integers in a list
-    (only once for each integer)."""
-    new = []
-    for item in my_list:
-        for x in new:
-            if x == item:
-                check = False
-            else:
-                check = True
-        if check:
-            new.append[item]
+    (only once for each integer)"""
+    sum = 0
+    for i in (my_list):
+        sum += my_list[i]
+    return sum
