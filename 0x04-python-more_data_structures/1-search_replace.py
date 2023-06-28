e90c84c0989157618cb550b2b74e14c2b9967b63
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-28 01:27:31 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """a function that replaces all occurrences of
+    an element by another in a new list"""
+    if not my_list:
+        return my_list
+    my_list = list(map(lambda x: x.replace(search, replace), my_list))
+    return my_list
