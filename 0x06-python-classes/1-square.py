15a5acd48dba0884eb216868879a3a4d8ff8d0c9
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-27 19:27:57 -0700

    new square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+class square():
+    """a square class"""
+    def __init__(self, size)
+        """ initialize the class
+        Args:
+            size: size of the square
+        """
+        self.__size = size
