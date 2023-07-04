747db3a8463251722f837653ce45ff52dcbf0768
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-03 18:30:42 -0700

    task 1.2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle():
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -19,11 +19,11 @@ class Rectangle():
             raise TypeError("width must be an integer")
         elif (value < 0):
             raise ValueError("width must be >= 0")
-    self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
