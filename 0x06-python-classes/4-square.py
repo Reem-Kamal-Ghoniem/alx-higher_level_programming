87b0a0e12e21715f53df63aa48dfa4d08453acb5
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-03 20:09:13 -0700

    setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square():
+    """square class with it's size"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
