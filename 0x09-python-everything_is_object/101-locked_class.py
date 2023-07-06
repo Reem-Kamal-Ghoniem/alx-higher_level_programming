8b311e455a3f90c71ee574e772d067775a9f4a75
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-05 18:11:07 -0700

    task 103

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-class LockedClass():
-    __slots__ = ['first_name']
+class LockedClass:
+    __slots__ = ('first_name',)
 
-    def __init__(self):
-        pass
+    def __setattr__(self, name, value):
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError("Cannot create new instance attributes")
+        super().__setattr__(name, value)
