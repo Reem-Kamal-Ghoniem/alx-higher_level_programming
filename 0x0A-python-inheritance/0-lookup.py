8bed24f48bd0aea52b77142258579f1e87e2f3a8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-10 16:59:39 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""a function that returns the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
+    """returns a list of objects"""
     return (dir(obj))
