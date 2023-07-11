edda7f0cbae24a16fcfd53b5d68b054ac6e8bc54
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-10 19:05:16 -0700

    task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""a function that returns True if the object is an instance of a class"""
+
 
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
