11e7d8666569fd692cd06e7030a5863fb115785e
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-10 19:22:12 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""file that wil Improve Geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """representing a rectangle"""
+
+    def __init__(self, width, height):
+        """define w, h"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
