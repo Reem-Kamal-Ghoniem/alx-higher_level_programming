a3be7cd684dc8a455d7cb1b70052f8077dbb0655
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-10 19:11:13 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""file that wil Improve Geometry"""
+
+
+class BaseGeometry:
+    """class for the basegeometry"""
+
+    def area(self):
+        """function for area"""
+        raise Exception("area() is not implemented")
