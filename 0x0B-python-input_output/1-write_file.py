2af03c04b420ba4c501ae11f68394405b8b68ae9
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-12 14:25:14 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""defines a function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """a function that writes a string to a text file
+    (UTF8) and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
