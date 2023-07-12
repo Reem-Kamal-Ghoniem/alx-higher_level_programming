72131ad2c841668829eea247971144a48f1e37b8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-12 15:11:21 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""a file that defines the function load_from_json_file"""
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object from a “JSON file”"""
+    with open(filename) as f:
+        return json.load(f)
