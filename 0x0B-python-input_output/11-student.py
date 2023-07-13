7b3e75c9097a325a81b72b857418f3c787a9a9e3
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-12 18:16:41 -0700

    task 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -19,11 +19,13 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None:
+        """ retrieves a dictionary representation of a Student instance"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
         for k, v in json.items():
             setattr(self, k, v)
