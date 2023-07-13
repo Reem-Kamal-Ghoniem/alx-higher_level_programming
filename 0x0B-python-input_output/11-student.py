487a120ba327d90f780e5ba8ff322c4bfc686f64
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-12 18:14:32 -0700

    task 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 
-"""defines class student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """a class Student that defines a student by:
+    """Represent a student."""
 
-    Public instance attributes:
-    first_name
-    last_name
-    age
-    """
     def __init__(self, first_name, last_name, age):
+        """a class Student that defines a student by:
+
+        Public instance attributes:
+        first_name
+        last_name
+        age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self, attrs=None:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-     def reload_from_json(self, json):
-         for k, v in json.items():
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
             setattr(self, k, v)
