61d56b4bcac73e99614a6cf97c1192ae5a59a9b9
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-12 19:10:08 -0700

    task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""defines a function that appends to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ a function that inserts a line of text to a file,
+    after each line containing a specific string"""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
