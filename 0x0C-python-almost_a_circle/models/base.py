8824ffdf66f90c4d3b0d8a695a5c8f3333c08023
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-15 15:18:25 -0700

    trial

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a module contains calss named base"""
+import json
+import csv
+import os.path
+
+
+class Base:
+    """the base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """the Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
