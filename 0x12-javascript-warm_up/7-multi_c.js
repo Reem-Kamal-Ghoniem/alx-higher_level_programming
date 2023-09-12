188d464359f99366c6055d745399248232b4d221
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-11 19:13:00 -0700

    styling

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-let x = +process.argv[2];
-if (x){
-  for (let i = 0; i < x; i++) {
+if (+process.argv[2]) {
+  for (let i = 0; i < (+process.argv[2]); i++) {
     console.log('C is fun');
   }
 } else {
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-const size = process.argv[2];
-if (!isNaN(size) && Number.isInteger(Number(size))) {
-  for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'X';
-    }
-    console.log(row);
-  }
+const { argv } = require('process');
+const size = Number(argv[2]);
+const repeat = 'X'.repeat(size);
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-  console.log("Missing size");
+  for (let i = 0; i < size; i++) {
+    console.log(repeat);
+  }
 }
