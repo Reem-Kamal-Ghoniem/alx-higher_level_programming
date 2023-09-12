82a76f44388006d5d583c9c615825b20acb8edb5
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-11 17:18:57 -0700

    task 2.edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
+const count = process.argv.length - 2;
 
-if (numArgs === 0){
-  console.log("No argument");
-} else if (numArgs === 1) {
-  console.log("Argument found");
-} else {console.log("Arguments found");}
+if (count === 0) {
+  console.log('No argument');
+} else if (count === 1) {
+  console.log('Argument found');
+} else { console.log('Arguments found'); }
