1d401f5f827170582cfbbd2ad95ca286279d1e82
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-11 18:20:03 -0700

    task 8.edit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const size = +process.argv[2];
+const size = process.argv[2];
 if (!isNaN(size) && Number.isInteger(Number(size))) {
   for (let i = 0; i < size; i++) {
     let row = '';
