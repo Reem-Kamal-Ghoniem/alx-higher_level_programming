b7f07790322716184686ee03d696d42aab1447f1
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-13 19:18:51 -0700

    task 100, 101

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table 
-SELECT score, name from second_table WHERE name IS NOT NULL order by score desc;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
