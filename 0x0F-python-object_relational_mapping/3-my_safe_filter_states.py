6d6ad85f0d8b2cf53922bd84b33c78b503ce948c
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-15 15:33:21 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name_ = sys.argv[4]
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+        )
+    cur = connection.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (name_,))
+    rows = cur.fetchone()
+    if rows is not None:
+        print(rows)
+
+    cur.close()
+    connection.close()
