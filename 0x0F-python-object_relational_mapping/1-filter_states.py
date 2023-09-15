16083a41a924d1c2729e677118650197aa76fe85
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-15 00:46:02 -0700

    selecting what starts with N only

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+        )
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    connection.close()
