d886825e1f343603212929443eb3627a46ba8fd7
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-17 19:02:39 -0700

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a script that lists all State objects that contain
-the letter a from the database hbtn_0e_6_usa"""
+"""a script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -8,10 +8,12 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.exit(1)
 
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, db_name, search_name =\
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db_name),
                            pool_pre_ping=True)
@@ -19,10 +21,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State)\
-        .filter(State.name.like('%a%')).order_by(State.id)
+    state = session.query(State).filter(State.name == search_name).first()
 
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
 
     session.close()
