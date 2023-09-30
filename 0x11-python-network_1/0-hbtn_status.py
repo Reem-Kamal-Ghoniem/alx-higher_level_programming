e1d4a485a7702f629bc63fb96a162c8b93aa29fb
Reem-Kamal-Ghoniem <reemkamal11@gmail.com>
2023-09-30 15:25:56 -0700

    fetching URL.edit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 from urllib import request
 
 url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    content_b = response.read()
-    content_S = content_b.decode('utf-8')
-    print("Body response:")
-    print(f"    - type: {type(content_bytes)}")
-    print(f"    - content: {content_bytes}")
-    print(f"    - utf8 content: {content_utf8}")
+if __name__ == "__main__":
+    with request.urlopen(url) as response:
+        content_b = response.read()
+        content_S = content_b.decode('utf-8')
+        print("Body response:")
+        print(f"    - type: {type(content_b)}")
+        print(f"    - content: {content_b}")
+        print(f"    - utf8 content: {content_S}")
