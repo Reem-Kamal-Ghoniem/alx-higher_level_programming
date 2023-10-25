0a9bf41d361158d33684f279a42914327d442e6f
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-10-24 19:59:10 -0700

    movies search .edit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,14 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
     const data = JSON.parse(body);
     let movieCount = 0;
     data.results.forEach((movie) => {
       if (movie.characters.includes(characterId)) {
         movieCount++;
-      }
-    });
-    console.log(movieCount);
-  } else {
-    console.error('Error:', error);
   }
+  console.log(movieCount);
 });
