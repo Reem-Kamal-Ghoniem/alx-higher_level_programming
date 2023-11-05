b219b6c670769a9e322a6e72cbb7cf90235cf127
Reem-Kamal-Ghoniem <reemkamal11@gmail.com>
2023-11-04 18:42:09 -0700

    posting emails.edit2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,11 +4,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
-    print("Your email is:", email)
-    print(response.text)
+    responce = requests.post(url, data={'email': email})
+    print(responce.text)
